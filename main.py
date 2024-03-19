35d2f7e6b3b7686bb8b8ff773318e49beb278dc9
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-03-19 21:57:26 +0800

    finishing sesi 1

--- main.py
+++ main.py
@@ -12,9 +12,16 @@ print(f''' Haaloo {nama_user} coba perhatikan goa berikut |_| |_| |_| ''')
 
 pilihan_user = int(input("Menurut kamu di goa nomor berapa RYU berada?? [1/2/3/4] = "))
 
-print(f"Pilihan kamu adalah {pilihan_user}")
+confirm_answer = input("Yakin dengan jawabanmu ? [y/n]")
 
-if pilihan_user == ryupy_position : 
-    print(f"SELAMAT {nama_user} KAMU MENANG!!!!, goa berada di nomor {ryupy_position}")
+if confirm_answer == "n" : 
+    print("Program dihentikan!!!!")
+    exit()
+elif confirm_answer == "y" :
+    if pilihan_user == ryupy_position : 
+        print(f"SELAMAT {nama_user} KAMU MENANG!!!!, goa berada di nomor {ryupy_position}")
+    else : 
+        print(f"{nama_user} KAMU KALAH ryupy tidak ada disitu, goa berada di nomor {ryupy_position}")
 else : 
-    print(f"{nama_user} KAMU KALAH? ryupy tidak ada disitu, goa berada di nomor {ryupy_position}")
+    print("program berakhir silahkan coba lagii")
+    exit()
